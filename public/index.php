4142b4f818084e1c77d0708190371edcc0854599
Thibault <thibaultfoure@gmail.com>
2021-10-07 12:18:40 +0200

    Ajout icones h3 skills & hobbies, renommage 'interests' en 'hobbies'car plus correct, coup de propre sur l'indentation des fichiers

--- public/index.php
+++ public/index.php
@@ -14,13 +14,11 @@
 
 <body>
     <header>
-
         <nav>
             <a href="#" class="myCV" id="myCV">
                 <h1>Bob L'Éponge</h1>
             </a>
             <a href="#navigation" class="linkList">
-
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="list" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                 </svg>
@@ -29,19 +27,19 @@
                 <a href="#" class="linkCloseBurger">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="closeBurger" viewBox="0 0 16 16">
                         <path d="M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z" />
-                    </svg></a>
+                    </svg>
+                </a>
                 <a href="#">À propos</a>
                 <a href="#">Formations</a>
                 <a href="#">Expériences</a>
                 <a href="#skills">Compétences</a>
-                <a href="#interests">Centres d'intérêt</a>
+                <a href="#hobbies">Centres d'intérêt</a>
             </div>
-
         </nav>
     </header>
+
     <section class="presentation">
         <div class="bloc">
-
             <img src="https://casoar.org/wp-content/uploads/2020/10/Bob-leponge.png" alt="Jolie éponge">
             <div class="title">
                 <h3>À propos</h3>
@@ -68,6 +66,7 @@
             </p>
         </div>
     </section>
+
     <section class="experience">
         <h3>Expériences</h3>
         <div class="experience-info">
@@ -82,21 +81,27 @@
             </p>
         </div>
     </section>
-    <section class="skillsAndInterests">
 
-
+    <section class="skillsAndHobbies">
         <h2 class="mantra">"Quoi de mieux que de servir des sourires ?"</h2>
         <div class="personalStuff">
             <div class="skills" id="skills">
-
                 <ul>
-                    <h3>Compétences</h3>
-                    <li class="skillsList">Pâtés de crabe
+                    <h3>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="iconsSkillsAndHobbies" viewBox="0 0 16 16">
+                            <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
+                            <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z" />
+                        </svg>
+                        <span class="spaceIconsh3">Compétences</span>
+                    </h3>
+                    <li class="skillsList">
+                        Pâtés de crabe
                         <div class="progressBar">
                             <div class="progressColor" id="crabSteak"></div>
                         </div>
                     </li>
-                    <li class="skillsList">Faire pouet avec ses pieds
+                    <li class="skillsList">
+                        Faire pouet avec ses pieds
                         <div class="progressBar">
                             <div class="progressColor" id="pouet" max="100" value="82"></div>
                         </div>
@@ -107,12 +112,14 @@
                             <div class="progressColor" id="client" max="100" value="67"></div>
                         </div>
                     </li>
-                    <li class="skillsList">Changer de forme à volonté
+                    <li class="skillsList">
+                        Changer de forme à volonté
                         <div class="progressBar">
                             <div class="progressColor" id="shapeChanger" max="100" value="88"></div>
                         </div>
                     </li>
-                    <li class="skillsList">Faire repousser ses membres
+                    <li class="skillsList">
+                        Faire repousser ses membres
                         <div class="progressBar">
                             <div class="progressColor" id="members" max="100" value="79"></div>
                         </div>
@@ -121,10 +128,15 @@
                 <img class="bobBurger" src="images/emojipng.com-4981643.png" alt="Bob burger">
             </div>
             <hr>
-            <div class="interests">
+            <div class="hobbies">
                 <img class="fishing" src="images/bobMéduse.png" alt="Bob filet méduse">
-                <h3 id="interests">Centres d'intérêt</h3>
                 <ul>
+                    <h3 id="hobbies">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="iconsSkillsAndHobbies" viewBox="0 0 16 16">
+                            <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z" />
+                        </svg>
+                        <span class="spaceIconsh3">Centres d'intérêt</span>
+                    </h3>
                     <li>Faire des bulles de savon avec Patrick</li>
                     <li>Attraper des méduses</li>
                     <li>Faire du karaté avec Sandy</li>
@@ -133,25 +145,30 @@
                 </ul>
             </div>
         </div>
-
     </section>
+
     <footer>
         <h2>Me contacter :</h2>
         <div class="contactMe">
             <a href="http://www.spontex.fr" target="_blank">
-                <h3 class="linkedin">LinkedIn : <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                <h3 class="linkedin">
+                    LinkedIn :
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
                         <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
-                    </svg></h3>
+                    </svg>
+                </h3>
             </a>
             <div class="mail">
                 <a href="mailto:bobeponge@bikinibottom.eau">
-                    <h3>Gmail : <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16">
+                    <h3>
+                        Gmail :
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16">
                             <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
-                        </svg></h3>
+                        </svg>
+                    </h3>
                 </a>
             </div>
         </div>
-
     </footer>
 
 
