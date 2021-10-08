214fa0bb59228fa88e528b0ee40c60158b540015
loick le sausse <rasmo@gmail.com>
2021-10-08 09:41:19 +0200

    modifier nom des formations

--- src/script.php
+++ src/script.php
@@ -2,15 +2,13 @@
 
 $formations = [
     ['title' => 'University burger flip', 'date' => '1988-1990', 'place' => 'Bikini Bottom', 'description' => 'Un apprentissage dans le flip de burger à l\'aide d\'un grand chef pendant 2 ans.'],
-    ['title' => 'Formation de l\'huitre ouverte', 'date' => '2018', 'place' => 'Occitan coastline', 'description' => 'Formation dans l\'ouverture d\'huitre.'],
-    ['title' => 'Crabe Croustillants', 'date' => '1990-2018', 'place' => 'Bikini Bottom', 'description' => 'Chef cuisiner,employés du mois à plusieur reprise et plusieur distinction comme badge du cuisinier qui fais les meilleurs pâtés de crabe'],
+    ['title' => 'Formation de l\'huitre ouverte', 'date' => '2018-2018', 'place' => 'Occitan coastline', 'description' => 'Formation dans l\'ouverture d\'huitre.'],
+    ['title' => 'École des bonnes manières', 'date' => '1986-1987', 'place' => 'Bikini Bottom', 'description' => 'Cours de savoir-vivre pour apprendre les geste à avoir en société'],É 
 ];
 
 
 $experiences = [
     ['title' => 'Crabe Croustillants', 'date' => '1990-2018', 'place' => 'Bikini Bottom', 'description' => 'Chef cuisiner,employés du mois à plusieur reprise et plusieur distinction comme badge du cuisinier qui fais les meilleurs pâtés de crabe'],
     ['title' => 'À l\'huitre luisante', 'date' => '2018-2021', 'place' => 'Bucket Angular', 'description' => 'Découverte d\'une véritable passion pour l\'ouverture d\'huitres.'],
-    ['title' => 'Crabe Croustillants', 'date' => '1990-2018', 'place' => 'Bikini Bottom', 'description' => 'Chef cuisiner,employés du mois à plusieur reprise et plusieur distinction comme badge du cuisinier qui fais les meilleurs pâtés de crabe'],
-
 ];
 
